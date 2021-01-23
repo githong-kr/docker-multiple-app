3c31e85a6966d19c3ef658dbc8a160028d52d8f9
githong@kakao.com <rkfaorl12!@>
2021-01-23 21:31:47 +0900

    first commit

---
+++ backend/db.js
@@ -0,0 +1,9 @@
+const mysql = require("mysql");
+const pool = mysql.createPool({
+    connectionLimit: 10,
+    host: 'mysql',
+    user: 'root',
+    password: '1q2w3e4r',
+    database: 'myapp'
+});
+exports.pool = pool;
